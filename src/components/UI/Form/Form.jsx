270ec6c584ef390fc11888adfb0493e8504f66b1
Douglas Feuser <douglas.feuser@gmail.com>
2021-01-15 18:03:56 -0300

    via cep integration

--- src/components/UI/Form/Form.jsx
+++ src/components/UI/Form/Form.jsx
@@ -1,14 +1,33 @@
 import React, { Fragment } from "react";
-import { Field, reduxForm } from "redux-form";
+import { Field, reduxForm, change } from "redux-form";
 import { Form } from "semantic-ui-react";
 
 const UserForm = props => {
-  const { handleSubmit, reset } = props;
+  const { handleSubmit, reset, setFieldValue } = props;
+
+  function onBlurCep(ev){
+
+    const { value } = ev.target;
+
+    const cep = value?.replace(/[^0-9]/g, '');
+
+    if (cep?.length !== 8) {
+      return;
+    }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        props.dispatch(change('user', 'endereco.rua', data.logradouro));
+        props.dispatch(change('user', 'endereco.bairro', data.bairro));
+        props.dispatch(change('user', 'endereco.cidade', data.localidade));
+      });
+
+  }
 
   return (
     <Fragment>
-
-      <Form onSubmit={handleSubmit}>
+      <Form name="user" onSubmit={handleSubmit}>
         <Form.Group widths="equal">
           <Field
             required
@@ -39,6 +58,7 @@ const UserForm = props => {
         name="endereco.cep"
         placeholder="CEP"
         width={7}
+        onBlur={(ev) => onBlurCep(ev, setFieldValue)}
         />
 
         <Form.Group widths="equal">
